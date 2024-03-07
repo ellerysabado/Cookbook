b8519b928983f9734a776cffe2926a0dff3a13c5
ellery <ellerysabado@gmail.com>
2024-03-06 20:55:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -20,11 +20,6 @@ function loadRecipes(data) {
                 <div class="card-body">
                     <p class="card-text">${title}</p>
                     <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                        </div>
-                        <small class="text-muted">DATE_HERE</small>
                     </div>
                 </div>
             </div>
