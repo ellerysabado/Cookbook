84515c07c703433d95f7294f01458b8b2f860d3f
ellery <ellerysabado@gmail.com>
2024-03-08 00:39:12 -0600

    change the index images and cards

--- index.js
+++ index.js
@@ -16,29 +16,37 @@ function loadRecipes(data) {
         let recipeCard = document.createElement("div");
         recipeCard.classList.add("col");
         recipeCard.innerHTML = `
-            <div id=${cardId} class="card shadow-sm">
-                <img src="${url}" alt="${title}" class="bd-placeholder-img card-img-top" width="400px" height="400px">
-                <div class="card-body">
-                    <p class="card-text">${title}</p>
+        <div class="container py-10">
+            <div class="row justify-content-center">
+                <div class="col">
+                <div class="card shadow-sm" style="background-color: #E78895;">
+
+                    <img src="${url}" alt="${title}" style="padding: 10px;">
+                    <div class="card-body text-center" style="background-color: #E78895;">
+                    <p class="card-text" style="font-size: 24px;"><strong>${title}</strong></p>
                     <div class="d-flex justify-content-between align-items-center">
+
+                    </div>
                     </div>
                 </div>
+                </div>
             </div>
+        </div>
         `;
 
         // Add event listener to the recipe card based on path
         if (path === "tacoring.html") {
-            recipeCard.addEventListener("click", function() {
+            recipeCard.addEventListener("click", function () {
                 window.location.href = path;
             });
         }
         if (path === "chickensquare.html") {
-            recipeCard.addEventListener("click", function() {
+            recipeCard.addEventListener("click", function () {
                 window.location.href = path;
             });
         }
         if (path === "pestopasta.html") {
-            recipeCard.addEventListener("click", function() {
+            recipeCard.addEventListener("click", function () {
                 window.location.href = path;
             });
         }
@@ -46,3 +54,16 @@ function loadRecipes(data) {
         recipeContainer.appendChild(recipeCard);
     });
 }
+
+//         <div class="container py-10">
+//   <div class="row justify-content-center" >
+//     <div class="col-6">
+//       <div class="card shadow-sm">
+//         <img src="${url}" alt="${title}" class="bd-placeholder-img card-img-top" style="height: 200px; object-fit: cover;">
+//         <div class="card-body text-center" style="height: 100%">
+//           <p class="card-text" style="height: 100%; display: flex; align-items: center; justify-content: center;">${title}</p>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// </div>
